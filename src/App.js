34ba2ad1839a215f8cfbe60dec276b226344efe9
Stef1202 <stefanieasaytono@gmail.com>
2024-06-21 08:52:14 +0800

    Added cursor effects

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Fluid } from '@whatisjery/react-fluid-distortion';
+import { EffectComposer } from '@react-three/postprocessing';
+import { Canvas } from '@react-three/fiber';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <Canvas
+        style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            height: '100vh',
+            width: '100vw',
+            background: '#000000',
+        }}>
+        <EffectComposer>
+            <Fluid />
+        </EffectComposer>
+    </Canvas>;
     </div>
   );
 }
