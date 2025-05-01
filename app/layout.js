72bc6f4bf50c4a071a01af8e4c0dcd2de864dba8
StefAsaytono <stef.asaytono@iest.com.ph>
2025-05-01 18:32:38 +0800

    add analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -19,10 +20,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
